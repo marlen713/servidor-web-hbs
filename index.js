47d76f3b2fd9fd02c4c15e3df27e98b037cf58af
marlen713 <ma000161@gmail.com>
2026-07-08 22:13:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import routerouter  from "router";
+
+
+// Creamos la aplicacion de Express
+const app = express()
+
+// Configuramos el entorno
+const PORT = 3000;
